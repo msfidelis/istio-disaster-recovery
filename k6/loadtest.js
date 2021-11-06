058dbf70973e49c76a7ee1e9dc0b0bd260374d61
Matheus Fidelis <matheus@nanoshots.com.br>
2021-11-06 11:05:01 -0300

    Serverless Remove

--- k6/loadtest.js
+++ k6/loadtest.js
@@ -2,14 +2,14 @@ import http from 'k6/http';
 
 export default  () => {
     const id = Math.random() +1 
-    const url = 'http://0.0.0.0:2000/orders/' + id;
+    const url = 'http://api.orders.k8s.cluster/orders/' + id;
 
     const payload = JSON.stringify({
     });
 
     const params = {
         headers: {
-        'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         },
     };
 
